f3ae8e13412142f0c0824e40d854ef677c17a429
Akxxxhil <1941012872.e.anshumanbiswal@gmail.com>
2026-01-05 09:54:45 +0530

    Add initial implementation of array filtering in array1.cpp

---
+++ Array/array1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int arr[]={1,3,5,7,9,10,11};
+    
+    for (int i=0;i< 7;i++){
+        if(arr[i] < 7){
+            cout << arr[i] << endl;
+        }
+    }
+}
